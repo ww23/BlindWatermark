922a73430ffce1640675a435ccb78edf0b73d9fe
ww23 <gywu233@gmail.com>
2018-12-21 10:56:39 +0800

    Add discrete cosine transform

--- README.md
+++ README.md
@@ -1,64 +1,90 @@
 
 # BlindWatermark
-[![Travis (.org)](https://img.shields.io/travis/USER/REPO.svg)](https://travis-ci.org/ww23/BlindWatermark)
+[![Build Status](https://travis-ci.org/ww23/BlindWatermark.svg?branch=master)](https://travis-ci.org/ww23/BlindWatermark)  
 
-Java 盲水印
-在图片上加肉眼无法分辨的水印
+在图片上加肉眼很难分辨的水印
 
 * 原理
      * Encode:  
-     原图 --- 傅立叶变换 ---> 频域图 + 水印 --- 逆变换 ---> 带水印图
+     原图 --- 变换 ---> 变换域 + 水印 --- 逆变换 ---> 带水印图
      * Decode(文字水印):  
-     带水印图 --- 傅立叶变换 ---> 频域图
+     带水印图 --- 变换 ---> 变换域
      * Decode(图片水印):  
-     原图 --- 傅立叶变换 --> 原图频域图  
-     带水印图 --- 傅立叶变换 ---> 频域图 - 原图频域图
+     原图 --- 变换 --> 原图变换域  
+     带水印图 --- 变换 ---> 变换域 - 原图变换域
 
-* 文件  
-    ├── gakki-img-dc.png   * 反解水印图(图片水印)  
-    ├── gakki-text-dc.png  * 反解水印图(文字水印)  
-    ├── gakki-wm-img.png   * 带图片盲水印的图  
-    ├── gakki-wm-text.png  * 带文字盲水印的图  
-    ├── gakki.png          * 原图  
-    └── wm.png             * 图片水印  
-
-### Usage:
+### Usage
     
     Usage: java -jar BlindWatermark.jar <commands> [args...]
         commands:
-                encode <option> <image-src>  <watermark-text>       <image-encoded(text)>
-                encode <option> <image-src>  <watermark-image>      <image-encoded(image)>
-                decode <image-encode(text)>  <image-decode>
-                decode <image-src>           <image-encoded(image)> <image-decode>
+            encode <option> <image-src> <watermark-text> <image-encoded(text)>
+            encode <option> <image-src> <watermark-image> <image-encoded(image)>
+            decode <option> <image-encode(text)> <image-decode>
+            decode <option> <image-src> <image-encoded(image)> <image-decode>
         options:
-                -i add image watermark
-                -t add text  watermark
-### Build:
+            -c discrete cosine transform
+            -f discrete fourier transform
+            -i image watermark
+            -t text  watermark
+        example:
+            encode -ft input.png watermark output.png
+### Build
 
 	gradle build
 
-### Demo:
+### Demo
 
     原图:
-![image](https://github.com/ww23/BlindWatermark/blob/master/gakki.png)
+![image](gakki-src.png)
 
-    加文字水印:
-    java -jar BlindWatermark.jar encode -t gakki.png mywife gakki-wm-text.png
-![image](https://github.com/ww23/BlindWatermark/blob/master/gakki-wm-text.png)
+    加文字水印(DFT):
+    java -jar BlindWatermark.jar encode -ft gakki-src.png mywife gakki-dft-text-ec.png
+![image](gakki-dft-text-ec.png)
 
-    加图片水印:
-    java -jar BlindWatermark.jar encode -i gakki.png wm.png gakki-wm-img.png
-![image](https://github.com/ww23/BlindWatermark/blob/master/wm.png)
-![image](https://github.com/ww23/BlindWatermark/blob/master/gakki-wm-img.png)
+    文字水印解码(DFT):
+    java -jar BlindWatermark.jar decode -ft gakki-dft-text-ec.png gakki-dft-text-dc.png
+![image](gakki-dft-text-dc.png)
 
-    文字水印解码:
-    java -jar BlindWatermark.jar decode gakki-wm-text.png gakki-text-dc.png
-![image](https://github.com/ww23/BlindWatermark/blob/master/gakki-text-dc.png)
+    加图片水印(DFT):
+    java -jar BlindWatermark.jar encode -fi gakki-src.png watermark.png gakki-dft-img-ec.png
+![image](gakki-dft-img-ec.png)
 
-    图片水印解码:
-    java -jar BlindWatermark.jar decode gakki.png gakki-wm-img.png gakki-img-dc.png
-![image](https://github.com/ww23/BlindWatermark/blob/master/gakki-img-dc.png)
+    图片水印解码(DFT):
+    java -jar BlindWatermark.jar decode -fi gakki-src.png gakki-dft-img-ec.png gakki-dft-img-dc.png
+![image](gakki-dft-img-dc.png)
 
-### Reference:
+    加文字水印(DCT):
+    java -jar BlindWatermark.jar encode -ct gakki-src.png mywife gakki-dct-text-ec.jpg
+![image](gakki-dct-text-ec.jpg)
 
-* [OpenCV](https://docs.opencv.org/3.4/d8/d01/tutorial_discrete_fourier_transform.html) Discrete Fourier Transform	
+    文字水印解码(DCT):
+    java -jar BlindWatermark.jar decode -ct gakki-dct-text-ec.jpg gakki-dct-text-dc.jpg
+![image](gakki-dct-text-dc.jpg)
+
+    加图片水印(DCT):
+    java -jar BlindWatermark.jar encode -ci gakki-src.png watermark.png gakki-dct-img-ec.jpg
+![image](gakki-dct-img-ec.jpg)
+
+    图片水印解码(DCT):
+    java -jar BlindWatermark.jar decode -ci gakki-src.png gakki-dct-img-ec.jpg gakki-dct-img-dc.jpg
+![image](gakki-dct-img-dc.jpg)
+
+### Reference
+
+* [OpenCV  Discrete Fourier Transform](https://docs.opencv.org/3.4/d8/d01/tutorial_discrete_fourier_transform.html)
+
+### License
+
+    Copyright 2018 ww23(https://github.com/ww23/BlindWatermark).
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/main/java/me/ww23/image/BlindWatermark.java
+++ src/main/java/me/ww23/image/BlindWatermark.java
@@ -1,7 +1,20 @@
-package me.ww23.image;
+/*
+ * Copyright 2018 ww23(https://github.com/ww23/BlindWatermark).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.bytedeco.javacv.CanvasFrame;
-import org.bytedeco.javacv.OpenCVFrameConverter;
+package me.ww23.image;
 
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgcodecs.*;
@@ -9,83 +22,79 @@ import static org.bytedeco.javacpp.opencv_imgproc.*;
 
 public class BlindWatermark {
 
+    private Option option = Option.DCT_IMAGE;
+
     public static void main(String[] args) {
 
-       if (args.length == 0) {
-           help();
-       }
+        if (args.length < 4) {
+            help();
+        }
 
-       String command = args[0];
+        BlindWatermark bwm = new BlindWatermark();
 
-       String arg1;
-       String arg2;
-       String arg3;
-       String arg4;
+        args[1] = args[1].substring(1);
+        switch (args[1]) {
+            case "tc":
+            case "ct":
+                bwm.option = Option.DCT_TEXT;
+                break;
+            case "ic":
+            case "ci":
+                bwm.option = Option.DCT_IMAGE;
+                break;
+            case "tf":
+            case "ft":
+                bwm.option = Option.DFT_TEXT;
+                break;
+            case "if":
+            case "fi":
+                bwm.option = Option.DFT_IMAGE;
+                break;
+            default:
+                help();
+        }
 
-       switch (command) {
-           case "encode":
-               arg1 = args[1];
-               if (args.length < 5) {
-                   help();
-               }
-               arg2 = args[2];
-               arg3 = args[3];
-               arg4 = args[4];
-               if (arg1.equals("-t")) {
-                   encode(arg2, arg3, arg4, true);
-               } else if (arg1.equals("-i")) {
-                   encode(arg2, arg3, arg4, false);
-               }
-               System.out.println("ENCODE SUCCESSFUL");
-               break;
-           case "decode":
-               arg1 = args[1];
-               arg2 = args[2];
-               if (args.length == 3) {
-                   decode(arg1, arg2);
-               } else if (args.length == 4) {
-                   arg3 = args[3];
-                   decode(arg1, arg2, arg3);
-               } else {
-                   help();
-               }
-               System.out.println("DECODE SUCCESSFUL");
-               break;
-           default:
-               help();
-       }
+        switch (args[0]) {
+            case "encode":
+                bwm.encode(args[2], args[3], args[4], bwm.option);
+                break;
+            case "decode":
+                switch (bwm.option) {
+                    case DCT_TEXT:
+                        bwm.decodeDCT(args[2], args[3]);
+                        break;
+                    case DFT_TEXT:
+                        bwm.decodeDFT(args[2], args[3]);
+                        break;
+                    case DFT_IMAGE:
+                        bwm.decodeDFT(args[2], args[3], args[4]);
+                        break;
+                    case DCT_IMAGE:
+                        bwm.decodeDCT(args[2], args[3], args[4]);
+                        break;
+                }
+                break;
+            default:
+                help();
+        }
+//        bwm.encode("gakki.png", "wm.png", "gakki-test.jpg", bwm.option);
+//        bwm.decodeDCT("gakki.png", "gakki-test.jpg","test.jpg");
+    }
 
-//        Mat srcImg = imread("gakki.png", CV_RGB2GRAY);
-/*
-        srcImg.convertTo(srcImg, CV_32F);
-
-        Mat result = new Mat(srcImg.size(), srcImg.type());
-        dct(srcImg, result);*/
-//        showMat(srcImg);
-
-//        encode("gakki.png", "wm.png", "gakki-test.png", false);
-//        decode("gakki.png", "gakki-test.png", "test.png");
+    public enum Option {
+        DFT_IMAGE, DFT_TEXT, DCT_IMAGE, DCT_TEXT
     }
 
     /**
      * 添加水印
      *
-     * @param image     原图
-     * @param watermark 水印
-     * @param output    水印为本 或 图片水印路径
+     * @param image     原图路径
+     * @param watermark 图片水印路径 或 水印文本
+     * @param output    加水印图片路径
      * @param option    false:图片水印 true:文字水印
      */
-    private static void encode(String image, String watermark, String output, Boolean option) {
-
-        //load image
+    private void encode(String image, String watermark, String output, Option option) {
         Mat srcImg = imread(image, CV_LOAD_IMAGE_COLOR);
-
-        if (srcImg.empty()) {
-            System.exit(1);
-        }
-
-        //merge color channels
-        MatVector newPlanes = new MatVector(3);
 
         //split color channels
         MatVector color = new MatVector(3);
@@ -93,36 +102,47 @@ public class BlindWatermark {
 
         MatVector[] planes = {new MatVector(2), new MatVector(2), new MatVector(2)};
         for (int i = 0; i < color.size(); i++) {
-            Mat comImg = startDFT(color.get(i));
-            if (option) {
-                addTextWaterMark(comImg, watermark);
-            } else {
-                addImageWaterMark(comImg, watermark);
+            Mat comImg = new Mat();
+            switch (option) {
+                case DFT_TEXT:
+                    comImg = startDFT(color.get(i));
+                    addMirrorTextWatermark(comImg, watermark);
+                    inverseDFT(comImg, planes[i]);
+                    break;
+                case DFT_IMAGE:
+                    comImg = startDFT(color.get(i));
+                    addMirrorImageWatermark(comImg, watermark);
+                    inverseDFT(comImg, planes[i]);
+                    break;
+                case DCT_TEXT:
+                    comImg = startDCT(color.get(i));
+                    addTextWatermark(comImg, watermark);
+                    inverseDCT(comImg, planes[i]);
+                    break;
+                case DCT_IMAGE:
+                    comImg = startDCT(color.get(i));
+                    addImageWatermark(comImg, watermark);
+                    inverseDCT(comImg, planes[i]);
+                    break;
             }
-            inverseDFT(comImg, planes[i]);
-            newPlanes.put(i, comImg);
+            color.put(i, comImg);
         }
 
-        Mat nImg = new Mat();
-        merge(newPlanes, nImg);
+        Mat res = new Mat();
+        merge(color, res);
 
-        imwrite(output, nImg);
+        imwrite(output, res);
     }
 
     /**
-     * 文本水印解码
+     * 文本水印解码 DFT
      *
      * @param wmImg  加了文本水印的图像
-     * @param output 图像中文本水印
+     * @param output 文本水印
      */
-    private static void decode(String wmImg, String output) {
-        Mat decImg = imread(wmImg, CV_LOAD_IMAGE_GRAYSCALE);
+    private void decodeDFT(String wmImg, String output) {
+        Mat decImg = startDFT(imread(wmImg, CV_LOAD_IMAGE_GRAYSCALE));
 
-        if (decImg.empty()) {
-            System.exit(1);
-        }
-
-        decImg = startDFT(decImg);
         MatVector newPlanes = new MatVector(2);
         Mat mag = new Mat();
         split(decImg, newPlanes);
@@ -137,29 +157,59 @@ public class BlindWatermark {
     }
 
     /**
-     * 图片水印解码
+     * 图片水印解码 DFT
      *
-     * @param srcImg 原图(没有加水印)
+     * @param srcImg 原图
      * @param wmImg  加了图片水印的图像
      * @param output 图像中的水印
      */
-    private static void decode(String srcImg, String wmImg, String output) {
-        Mat src = imread(srcImg, CV_LOAD_IMAGE_GRAYSCALE);
-        Mat wm = imread(wmImg, CV_LOAD_IMAGE_GRAYSCALE);
+    private void decodeDFT(String srcImg, String wmImg, String output) {
+        Mat src = startDFT(imread(srcImg, CV_LOAD_IMAGE_GRAYSCALE));
+        Mat wm = startDFT(imread(wmImg, CV_LOAD_IMAGE_GRAYSCALE));
 
-        if (src.empty() || wm.empty()) {
-            System.exit(1);
-        }
-
-        startDFT(src).convertTo(src, CV_32F);
-        startDFT(wm).convertTo(wm, CV_32F);
-
-        //srcImg - wmImg
         subtract(wm, src, wm);
 
         MatVector newPlanes = new MatVector(2);
         split(wm, newPlanes);
         wm = newPlanes.get(0);
+
+        imwrite(output, wm);
+    }
+
+    /**
+     * 文本水印解码 DCT
+     *
+     * @param wmImg  加了文本水印的图像
+     * @param output 文本水印
+     */
+    private void decodeDCT(String wmImg, String output) {
+        Mat decImg = startDCT(imread(wmImg, CV_LOAD_IMAGE_GRAYSCALE));
+
+        decImg.convertTo(decImg, COLOR_RGB2HSV);
+        Mat low = new Mat(new Scalar(1, 1, 1, 0));
+        Mat upp = new Mat(new Scalar(5, 5, 5, 0));
+        inRange(decImg, low, upp, decImg);
+
+        normalize(decImg, decImg, 0, 255, NORM_MINMAX, CV_8UC1, null);
+        imwrite(output, decImg);
+    }
+
+    /**
+     * 图片水印解码 DCT
+     *
+     * @param srcImg 原图路径
+     * @param wmImg  加了图片水印的图像路径
+     * @param output 输出水印路径
+     */
+    private void decodeDCT(String srcImg, String wmImg, String output) {
+        Mat src = startDCT(imread(srcImg, CV_LOAD_IMAGE_GRAYSCALE));
+        Mat wm = startDCT(imread(wmImg, CV_LOAD_IMAGE_GRAYSCALE));
+
+        subtract(wm, src, wm);
+
+        wm.convertTo(wm, CV_8U);
+
+        equalizeHist(wm, wm);
 
         imwrite(output, wm);
     }
@@ -177,7 +227,6 @@ public class BlindWatermark {
         planes.put(0, srcImg);
         planes.put(1, Mat.zeros(srcImg.size(), CV_32F).asMat());
         merge(planes, comImg);
-//        dct(comImg, comImg);
         dft(comImg, comImg);
         return comImg;
     }
@@ -189,10 +238,132 @@ public class BlindWatermark {
      * @param planes 图像变量
      */
     private static void inverseDFT(Mat comImg, MatVector planes) {
-//        idct(comImg, comImg);
         idft(comImg, comImg);
         split(comImg, planes);
         normalize(planes.get(0), comImg, 0, 255, NORM_MINMAX, CV_8UC3, null);
+    }
+
+    /**
+     * 将图像进行DCT
+     *
+     * @param srcImg 源图像
+     * @return 转化后的图像
+     */
+    private static Mat startDCT(Mat srcImg) {
+        srcImg.convertTo(srcImg, CV_32F);
+        dct(srcImg, srcImg);
+        return srcImg;
+    }
+
+    /**
+     * DCT逆变换
+     *
+     * @param comImg DCT后的图像
+     * @param planes 图像变量
+     */
+    private static void inverseDCT(Mat comImg, MatVector planes) {
+        idct(comImg, comImg);
+        split(comImg, planes);
+        normalize(planes.get(0), comImg, 0, 255, NORM_MINMAX, CV_8UC3, null);
+    }
+
+    /**
+     * 添加文本水印
+     *
+     * @param comImg    图像
+     * @param watermark 水印
+     */
+    private static void addTextWatermark(Mat comImg, String watermark) {
+        putText(comImg, watermark,
+                new Point(comImg.size().width() / 4, comImg.size().height() / 4),
+                CV_FONT_HERSHEY_COMPLEX, 1.0,
+                new Scalar(2, 2, 2, 0), 2, 8, false);
+    }
+
+    /**
+     * 添加对称的文本水印
+     *
+     * @param comImg    图像
+     * @param watermark 水印
+     */
+    private static void addMirrorTextWatermark(Mat comImg, String watermark) {
+        Scalar s = new Scalar(0, 0, 0, 0);
+        Point p = new Point(comImg.size().width() / 3, comImg.size().height() / 3);
+
+        putText(comImg, watermark, p, CV_FONT_HERSHEY_COMPLEX, 1.0, s, 3,
+                8, false);
+        //旋转图片
+        flip(comImg, comImg, -1);
+
+        putText(comImg, watermark, p, CV_FONT_HERSHEY_COMPLEX, 1.0, s, 3,
+                8, false);
+        flip(comImg, comImg, -1);
+    }
+
+    /**
+     * 添加图片水印
+     *
+     * @param comImg    图像
+     * @param watermark 图片水印路径
+     */
+    private static void addImageWatermark(Mat comImg, String watermark) {
+        Mat wm = imread(watermark, CV_LOAD_IMAGE_GRAYSCALE);
+        wm.convertTo(wm, CV_32F);
+        createImageWaterMark(comImg, wm);
+        addWeighted(wm, 0.01, comImg, 1, 0.0, comImg);
+    }
+
+    /**
+     * 添加对称的图片水印
+     *
+     * @param comImg    图像
+     * @param watermark 图片水印路径
+     */
+    private static void addMirrorImageWatermark(Mat comImg, String watermark) {
+        Mat wm = imread(watermark, CV_LOAD_IMAGE_GRAYSCALE);
+        MatVector planes = new MatVector(2);
+        wm.convertTo(wm, CV_32F);
+
+        createMirrorImageWaterMark(comImg, wm);
+
+        planes.put(0, wm);
+        planes.put(1, wm);
+        merge(planes, wm);
+
+        addWeighted(wm, 0.5, comImg, 1, 0.0, comImg);
+    }
+
+    /**
+     * 将水印扩展成原图的大小
+     *
+     * @param comImg    原图
+     * @param watermark 水印图
+     */
+    private static void createImageWaterMark(Mat comImg, Mat watermark) {
+        copyMakeBorder(watermark, watermark,
+                comImg.rows() / 8, comImg.rows() - watermark.rows() - comImg.rows() / 8,
+                comImg.cols() / 8, comImg.cols() - watermark.cols() - comImg.cols() / 8,
+                BORDER_CONSTANT, Scalar.all(0));
+    }
+
+    /**
+     * 将水印对称并扩展成原图的大小
+     *
+     * @param comImg    原图
+     * @param watermark 水印图
+     */
+    private static void createMirrorImageWaterMark(Mat comImg, Mat watermark) {
+        MatVector combine = new MatVector(2);
+        Mat wm = new Mat();
+
+        copyMakeBorder(watermark, watermark, 0, comImg.rows() / 2 - watermark.rows(),
+                0, comImg.cols() - watermark.cols(), BORDER_CONSTANT, Scalar.all(0));
+
+        combine.put(0, watermark);
+        flip(watermark, wm, -1);
+        combine.put(1, wm);
+
+        vconcat(combine, watermark);
     }
 
     /**
@@ -210,90 +381,20 @@ public class BlindWatermark {
         return padded;
     }
 
-    /**
-     * 添加文本水印
-     *
-     * @param comImg 频谱图
-     */
-    private static void addTextWaterMark(Mat comImg, String watermark) {
-
-        Scalar s = new Scalar(0x00, 0);
-        Point p = new Point(comImg.size().width() / 3, comImg.size().height() / 3);
-
-        // add text
-        putText(comImg, watermark, p, CV_FONT_HERSHEY_COMPLEX, 1.5, s, 3,
-                20, false);
-        // 旋转图片
-        flip(comImg, comImg, -1);
-
-        putText(comImg, watermark, p, CV_FONT_HERSHEY_COMPLEX, 1.5, s, 3,
-                20, false);
-        flip(comImg, comImg, -1);
-    }
-
-    /**
-     * 添加图片水印
-     *
-     * @param comImg 频谱图
-     */
-    private static void addImageWaterMark(Mat comImg, String watermark) {
-        Mat wm = imread(watermark, CV_LOAD_IMAGE_GRAYSCALE);
-        MatVector planes = new MatVector(2);
-        wm.convertTo(wm, CV_32F);
-        if (wm.empty()) {
-            System.exit(1);
-        }
-        //same size
-        createWaterMark(comImg, wm);
-        //水印编码
-        //...
-
-        //same channel
-        planes.put(0, wm);
-        planes.put(1, wm);
-        merge(planes, wm);
-
-        //add mark
-        addWeighted(wm, 0.5, comImg, 1, 0.0, comImg);
-    }
-
-    /**
-     * 将图片水印对称并扩展成原图的大小
-     *
-     * @param comImg 原图频域图
-     * @param wm     水印图
-     */
-    private static void createWaterMark(Mat comImg, Mat wm) {
-        MatVector combine = new MatVector(2);
-        Mat iwm = new Mat();
-
-        copyMakeBorder(wm, wm, 0, comImg.rows() / 2 - wm.rows(),
-                0, comImg.cols() - wm.cols(), BORDER_CONSTANT, Scalar.all(0));
-        combine.put(0, wm);
-        flip(wm, iwm, -1);
-        combine.put(1, iwm);
-        vconcat(combine, wm);
-    }
-
-    /**
-     * 显示 Mat
-     * @param matrix 要显示的 Mat
-     */
-    private static void showMat(Mat matrix) {
-        CanvasFrame canvas = new CanvasFrame(matrix.toString());
-        canvas.showImage(new OpenCVFrameConverter.ToMat().convert(matrix));
-    }
-
     private static void help() {
-        System.out.println("Usage: java -jar InvisibleWatermark.jar <commands> [args...] \n" +
+        System.out.println("Usage: java -jar BlindWatermark.jar <commands> [args...] \n" +
                 "   commands: \n" +
-                "       encode <option> <image-src>  <watermark-text>       <image-encoded(text)>\n" +
-                "       encode <option> <image-src>  <watermark-image>      <image-encoded(image)>\n" +
-                "       decode <image-encode(text)>  <image-decode>\n" +
-                "       decode <image-src>           <image-encoded(image)> <image-decode>\n" +
+                "       encode <option> <image-src> <watermark-text> <image-encoded(text)>\n" +
+                "       encode <option> <image-src> <watermark-image> <image-encoded(image)>\n" +
+                "       decode <option> <image-encode(text)> <image-decode>\n" +
+                "       decode <option> <image-src> <image-encoded(image)> <image-decode>\n" +
                 "   options: \n" +
-                "       -i add image watermark\n" +
-                "       -t add text  watermark\n"
+                "       -c discrete cosine transform\n" +
+                "       -f discrete fourier transform\n" +
+                "       -i image watermark\n" +
+                "       -t text  watermark\n" +
+                "   example: \n" +
+                "       encode -ft foo.png test bar.png"
         );
         System.exit(-1);
     }
